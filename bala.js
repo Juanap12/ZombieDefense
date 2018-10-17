f3e1aa47a23d39e4ec33b7025a9d933c49fd3aa3
imwiwiim90 <imwiwiim90@gmail.com>
2018-10-17 14:59:54 -0500

    fixing variable named "velocidad"

--- bala.js
+++ bala.js
@@ -1,18 +1,19 @@
 class Bala {
     constructor(angulo){
-        this.angulo = angulo
+        this.angulo = angulo;
+        this.velocidad = 0.1;
 
-        let geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1)
-        let material = new THREE.MeshNormalMaterial()
-        this.cube = new THREE.Mesh( geometry, material)
-        scene.add(this.cube)
+        let geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1);
+        let material = new THREE.MeshNormalMaterial();
+        this.cube = new THREE.Mesh( geometry, material);
+        scene.add(this.cube);
 
-        this.velocidad = 0;
+        this.distancia = 0;
     }
 
     actualizar(){
-        this.cube.position.x = this.velocidad * Math.cos(this.angulo)
-        this.cube.position.y = this.velocidad * Math.sin(this.angulo)
-        this.velocidad += 0.1
+        this.cube.position.x = this.distancia * Math.cos(this.angulo);
+        this.cube.position.y = this.distancia * Math.sin(this.angulo);
+        this.distancia += this.velocidad;
     }
 }
