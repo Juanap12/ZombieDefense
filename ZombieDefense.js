9bc3edcca54381482dae7660194f3b574b058ae1
Juanap12 <juanpablocj@gmail.com>
2018-10-30 00:21:48 -0500

    Se añadió variable booleana para diferenciar cuando se está probando con Arduino

--- ZombieDefense.js
+++ ZombieDefense.js
@@ -1,51 +1,57 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var serv = require('http').Server(app);
+var serv = require("http").Server(app);
 
-app.get('/',function(req, res) {
-    res.sendFile(__dirname + '/client/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/client/index.html");
 });
-app.use('/client',express.static(__dirname + '/client'));
+app.use("/client", express.static(__dirname + "/client"));
 serv.listen(2000);
 console.log("Server started.");
 
-var io = require('socket.io')(serv,{});
+var io = require("socket.io")(serv, {});
 var csocket = undefined;
-io.sockets.on('connection', function(socket){
-    console.log('socket connection');
-    csocket = socket;
+io.sockets.on("connection", function(socket) {
+  console.log("socket connection");
+  csocket = socket;
 });
 
 var five = require("johnny-five"),
-    board, botton, potentiometer;
-board = new five.Board();
+  board,
+  botton,
+  potentiometer;
 
-board.on("ready", function() {
-  button = new five.Button(2);
-  potentiometer = new five.Sensor({
-    pin: "A0",
-    freq: 250
-  });
+let probarSinArduino = true;
 
-   this.repl.inject({
-     pot: potentiometer,
-     button: button
-   });
+if (!probarSinArduino) {
+  board = new five.Board();
 
-   potentiometer.on("data", function() {
+  board.on("ready", function() {
+    button = new five.Button(2);
+    potentiometer = new five.Sensor({
+      pin: "A0",
+      freq: 250
+    });
+
+    this.repl.inject({
+      pot: potentiometer,
+      button: button
+    });
+
+    potentiometer.on("data", function() {
       var pot = this;
       if (csocket != undefined)
-        csocket.emit('serverMsg',{
-           msg: pot.value,
-      });
+        csocket.emit("serverMsg", {
+          msg: pot.value
+        });
+    });
+
+    button.on("down", function() {
+      console.log("ON");
+      if (csocket != undefined)
+        csocket.emit("serverMsg", {
+          msg: "on"
+        });
+    });
   });
-
-   button.on("down", function() {
-     console.log("ON");
-     if(csocket != undefined)
-      csocket.emit('serverMsg',{
-        msg: "on",
-      });
-   });
-
-});
+}
